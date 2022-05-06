152b229531458d1935f4e43de18fee900c691362
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-05-06 18:52:40 +0400

    Created adaptation for destroy zone

---
+++ Assets/Scripts/View/DestroyZoneSize.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace View
+{
+    public class DestroyZoneSize : MonoBehaviour
+    {
+        [SerializeField] private float percentDestroyZoneSize;
+        private void Start()
+        {
+            var sizeX = GetWidthByScreen();
+            SetWidthByScreen(sizeX);
+            var sizeY = GetHeightByScreen();
+            SetHeightByScreen(sizeY);
+        }
+
+        private void SetWidthByScreen(float destroyZoneSize)
+        {
+            var localScale = transform.localScale;
+            localScale.x = destroyZoneSize;
+            transform.localScale = localScale;
+        }
+        private void SetHeightByScreen(double destroyZoneSize)
+        {
+            var localScale = transform.localScale;
+            localScale.y = (float)destroyZoneSize;
+            transform.localScale = localScale;
+        }
+
+        private float GetWidthByScreen()
+        {
+            var width = Screen.width;
+            var height = Screen.height;
+            var leftDownCorner = new Vector2(0, height);
+            var rightDownCorner = new Vector2(width, height);
+            var worldLeftDownCorner = Camera.main.ScreenToWorldPoint(leftDownCorner);
+            var worldRightDownCorner = Camera.main.ScreenToWorldPoint(rightDownCorner);
+            var destroyZoneSize = (worldRightDownCorner - worldLeftDownCorner).x;
+            return destroyZoneSize;
+        }
+
+        private double GetHeightByScreen()
+        {
+            var height = Screen.height;
+            var leftDownCorner = new Vector2(0, height);
+            var leftUpCorner = new Vector2(0, 0);
+            var worldLeftDownCorner = Camera.main.ScreenToWorldPoint(leftDownCorner);
+            var worldLeftUpCorner = Camera.main.ScreenToWorldPoint(leftUpCorner);
+            var destroyZoneSize = (worldLeftDownCorner - worldLeftUpCorner).y * percentDestroyZoneSize;
+            return destroyZoneSize;
+        }
+    }
+}
