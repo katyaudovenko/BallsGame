5a066b1bf4d9faab38b12f1705b0e7e99be56354
katyaudovenko <katya.udovenko.1996@gmail.com>
2022-07-20 13:27:40 +0400

    Created settings window

---
+++ Assets/Scripts/Controller/Windows/Settings/SettingsWindow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controller.Windows.Settings
+{
+    public class SettingsWindow : BaseWindow
+    {
+        [SerializeField] private Button closeButton;
+
+        public override void OnInitialize()
+        {
+            base.OnInitialize();
+            AddButtonListeners();
+        }
+
+        private void AddButtonListeners() => 
+            closeButton.onClick.AddListener(OnClickCloseButton);
+        
+        private void OnClickCloseButton() => 
+            OnClose.Invoke(typeof(SettingsWindow), this);
+    }
+}
